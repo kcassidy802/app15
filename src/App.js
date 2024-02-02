179884ea5e3f00f108f14b32511b1cc72bdcdb92
Kyle Cassidy <146395281+kcassidy802@users.noreply.github.com>
2024-02-02 12:35:10 -0800

    okay final change

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
+// Using the Houses API, or any open API of your choice you can find online, create a single page that allows for all 4 CRUD operations to be performed on a resource from the API.
+// Create a React component (or more, if needed) to represent the resource.
+// Make all forms and other necessary UI pieces their own components as reasonable.
+
+
+
+
+
+
 import React, { useState, useEffect } from 'react'; //importing all my files to the app.js
 import axios from 'axios';
 import ContactList from './components/ContactList';
